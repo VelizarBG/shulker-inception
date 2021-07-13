2556becb46c27078fdccfd1e3503564a053b6b2a
Velizar <velizarmc@abv.bg>
2021-07-13 08:15:06 +0300

    Add functionality

---
+++ src/main/java/com/velizarbg/shulker_inception/mixins/ModifyNestingCheckMixin.java
@@ -0,0 +1,15 @@
+package com.velizarbg.shulker_inception.mixins;
+
+import net.minecraft.screen.slot.ShulkerBoxSlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ShulkerBoxSlot.class)
+public abstract class RemoveNestingCheckMixin {
+    @Inject(method = "canInsert", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;canBeNested()Z"), cancellable = true)
+    private void injected(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
